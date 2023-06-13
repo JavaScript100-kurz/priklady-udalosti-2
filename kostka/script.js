a7cbda07dda0e59a7bea8a5af1f15806fc663a6f
Luděk Roleček <ludek@rolecek.cz>
2023-06-13 17:43:44 +0200

    Aktualizace podkladů

---
+++ kostka/script.js
@@ -0,0 +1,7 @@
+/*
+Při kliknutí na tlačítko vygeneruj náhodné číslo od 1 do 6.
+Změň obrázek na stránce podle toho, jaké číslo padne.
+Podle toho, kolik na kostce padlo, do textu pod tlačítkem napiš:
+- Hodil jsi X, házej znovu.
+- Šestka! Vyhrál jsi.
+*/
